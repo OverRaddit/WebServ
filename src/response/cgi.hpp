aae6c08ebe26e4e1103e9951c0e440f99f210f38
feedbaek <nicek789@naver.com>
2022-09-02 08:46:40 +0900

    response make

---
+++ src/response/cgi.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cgi.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 17:13:43 by minskim2          #+#    #+#             */
+/*   Updated: 2022/08/28 17:59:53 by minskim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CGI_HPP
+# define CGI_HPP
+
+#include <unistd.h>
+#include <cstring>
+#include <string>
+#include <fcntl.h>
+
+int run_cgi();
+
+#endif
---
+++ src/response/response.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   response.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 17:13:25 by minskim2          #+#    #+#             */
+/*   Updated: 2022/08/28 21:08:11 by minskim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cgi.hpp"
+#include "response.hpp"
+
+int main(int argc, char **argv, char **envp) {
+	run_cgi();
+	//printf("하하\n");
+	//while (1) {
+	//}
+	//system("leaks test_response");
+	return 0;
+}
