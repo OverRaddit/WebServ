ed7ff840a3b8f753a819adcb9e8235d01094ff33
Gunwoo Shim <gshim@c4r4s7.42seoul.kr>
2022-09-14 18:48:14 +0900

    Feat: Response Module

--- src/response/response.cpp
+++ src/response/response.cpp
@@ -1,35 +1,32 @@
 #include "./Response.hpp"
 
-bool Response::initFlag = false;
-
 Response::Response() {
-	if (Response::initFlag == false)
-		this.ResponseInit();
 }
+
+map<int, string> Response::m_status_description;
 
 Response::~Response() {
 }
 
 void Response::ResponseInit() {
-	this->m_status_description[200] = "200 OK";
-	this->m_status_description[201] = "201 Created";
-	this->m_status_description[204] = "204 No Content";
-	this->m_status_description[300] = "300 Multiple Choices";
-	this->m_status_description[301] = "301 Moved Permanently";
-	this->m_status_description[302] = "302 Found";
-	this->m_status_description[303] = "303 See Other";
-	this->m_status_description[307] = "307 Temporary Redirect";
-	this->m_status_description[400] = "400 Bad Request";
-	this->m_status_description[404] = "404 Not Found";
-	this->m_status_description[405] = "405 Method Not Allowed";
-	this->m_status_description[408] = "408 Request Timeout";
-	this->m_status_description[411] = "411 Length Required";
-	this->m_status_description[413] = "413 Request Entity Too Large";
-	this->m_status_description[414] = "414 Request-URI Too Long";
-	this->m_status_description[500] = "500 Internal Server Error";
-	this->m_status_description[502] = "502 Bad Gateway";
-	this->m_status_description[505] = "505 HTTP Version Not Supported";
-	this->initFlag = true;
+	Response::m_status_description[200] = "200 OK";
+	Response::m_status_description[201] = "201 Created";
+	Response::m_status_description[204] = "204 No Content";
+	Response::m_status_description[300] = "300 Multiple Choices";
+	Response::m_status_description[301] = "301 Moved Permanently";
+	Response::m_status_description[302] = "302 Found";
+	Response::m_status_description[303] = "303 See Other";
+	Response::m_status_description[307] = "307 Temporary Redirect";
+	Response::m_status_description[400] = "400 Bad Request";
+	Response::m_status_description[404] = "404 Not Found";
+	Response::m_status_description[405] = "405 Method Not Allowed";
+	Response::m_status_description[408] = "408 Request Timeout";
+	Response::m_status_description[411] = "411 Length Required";
+	Response::m_status_description[413] = "413 Request Entity Too Large";
+	Response::m_status_description[414] = "414 Request-URI Too Long";
+	Response::m_status_description[500] = "500 Internal Server Error";
+	Response::m_status_description[502] = "502 Bad Gateway";
+	Response::m_status_description[505] = "505 HTTP Version Not Supported";
 }
 
 int	Response::getStatusCode() {
@@ -58,10 +55,14 @@ void Response::setContent(string content) {
 	this->m_content = content;
 }
 
+void Response::setCgiResult(string ret) {
+	this->m_cgiResult = ret;
+}
+
 string Response::makeHeaders() {
 	string result = "";
 
-	result.append("HTTP/1.1 " + this->m_status_code + " " + this->m_status_description[this->m_status_code] + "\r\n");
+	result.append("HTTP/1.1 " + to_string(this->m_status_code) + " " + this->m_status_description[this->m_status_code] + "\r\n");
 	for (map<string, string>::iterator i = this->m_headers.begin(); i != this->m_headers.end(); i++)
 		result.append((*i).first + ": " + (*i).second + "\r\n");
 	result.append("\r\n");
@@ -69,9 +70,8 @@ string Response::makeHeaders() {
 	return result;
 }
 
-string Response::makeContent() {
+void Response::makeContent() {
 	string result = "";
-
 	result.append("<!DOCTYPE html><html><head><meta charset=\"UTF-8\"/><title>webserv</title></head>");
 	result.append("<body>");
 	result.append("<h3>" + this->m_cgiResult + "</h3>");
@@ -84,86 +84,6 @@ string Response::makeContent() {
 
 string Response::getHttpResponse() {
 	string result = this->makeHeaders();
-	result += this->makeContent();
+	result += this->getContent();
 	return result;
 }
-
-//void Response::makeStatus200(int code) {
-//	if (code == 200) {
-
-//	} else if (code == 201) {
-
-//	} else if (code == 202) {
-
-//	} else if (code == 203) {
-
-//	} else if (code == 204) {
-
-//	} else if (code == 205) {
-
-//	} else if (code == 206) {
-//	}
-//}
-//void Response::makeStatus300(int code) {
-//	if (code == 300) {
-
-//	} else if (code == 301) {
-
-//	} else if (code == 302) {
-
-//	} else if (code == 303) {
-
-//	} else if (code == 304) {
-
-//	} else if (code == 305) {
-
-//	} else if (code == 306) {
-
-//	} else if (code == 307) {
-
-//	}
-
-//}
-//void Response::makeStatus400(int code) {
-//	if (code == 400) {
-
-//	} else if (code == 402) {
-
-//	} else if (code == 403) {
-
-//	} else if (code == 404) {
-
-//	} else if (code == 404) {
-
-//	} else if (code == 405) {
-
-//	} else if (code == 406) {
-
-//	} else if (code == 408) {
-//	} else if (code == 409) {
-//	} else if (code == 410) {
-//	} else if (code == 411) {
-//	} else if (code == 413) {
-//	} else if (code == 414) {
-//	} else if (code == 415) {
-//	} else if (code == 417) {
-//	} else if (code == 426) {
-
-//	}
-
-//}
-//void Response::makeStatus500(int code) {
-//	if (code == 500) {
-
-//	} else if (code == 501) {
-
-//	} else if (code == 502) {
-
-//	} else if (code == 503) {
-
-//	} else if (code == 504) {
-
-//	} else if (code == 505) {
-
-//	}
-//}
