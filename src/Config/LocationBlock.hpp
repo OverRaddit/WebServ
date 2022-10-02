07f112c3e2e713b4d6713d2ab3d785bcd2f3b68f
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-10-02 22:18:44 +0900

    Fix : Redirection 구현

--- src/Config/LocationBlock.hpp
+++ src/Config/LocationBlock.hpp
@@ -3,7 +3,6 @@
 
 # include <iostream>
 # include <string>
-# include <sstream>
 #include <vector>
 
 using namespace std;
@@ -17,18 +16,21 @@ public:
 	void	setUploadDirectory(string loc_block, size_t pos);
 	void	setRootDir(string loc_block, size_t pos);
 	void	setAutoIndex(string loc_block, size_t pos);
+	void	setRedirectionURL(string loc_block, size_t pos);
 
 	vector<string>	getValidMethod(void) const;
 	string			getUploadDirectory(void) const;
 	string			getRootDir(void) const;
 	long long 		getMaxBodySize(void) const;
 	bool			getAutoIndex(void) const;
+	string			getRedirectionURL(void) const;
 private:
 	vector<string>	m_valid_method;
 	long long		m_max_body_size;
 	string			m_upload_dir;
 	string			m_root_dir;
 	bool			m_autoindex;
+	string			m_redirection_url;
 };
 
 #endif
