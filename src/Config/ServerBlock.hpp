f785885a8dd6daa704a82379b327adbe2febfa7b
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-11-08 10:39:53 +0900

    Fix: 1) listen 포트 번호 여러 개 파싱해서 vector에 저장 2) type 지시어 구현 내용 전부 삭제

--- src/Config/ServerBlock.hpp
+++ src/Config/ServerBlock.hpp
@@ -3,6 +3,7 @@
 
 # include <map>
 #include <string>
+#include <vector>
 # include "LocationBlock.hpp"
 
 using namespace std;
@@ -11,7 +12,7 @@ class ServerBlock {
 public:
 	ServerBlock(void);
 
-	void	setPortNum(string port_num);
+	void	setPortNum(string port_nums);
 	void	setServerName(string server_name);
 	void	setRootDir(string root_dir);
 	void	setCgiTester(string cgi_tester);
@@ -19,18 +20,18 @@ public:
 	void	setLocationBlock(string loc_block);
 	void	setIndexFile(string file_name);
 
-	int		getPortNum(void) const;
-	string	getErrorPage(void) const;
-	string	getServerName(void) const;
-	string	getRootDir(void) const;
-	string	getCgiTester(void) const;
-	string	getCgiExtension(void) const;
-	string	getIndexFile(void) const;
+	vector<int>&	getPortNum(void) const;
+	string			getErrorPage(void) const;
+	string			getServerName(void) const;
+	string			getRootDir(void) const;
+	string			getCgiTester(void) const;
+	string			getCgiExtension(void) const;
+	string			getIndexFile(void) const;
 	const map<string, LocationBlock>&	getLocationBlocks(void) const;
 private:
 	map<string, LocationBlock>	m_loc_blocks;
 	string						m_error_page;
-	int							m_port_num;
+	vector<int>					m_port_nums;
 	string						m_server_name;
 	string						m_root_dir;
 	string						m_cgi_tester;
